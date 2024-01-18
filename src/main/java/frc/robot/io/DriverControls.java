27675223f0f0ee8f1a5e0ae2d56fdecd95697752
Kesavan Rangarajan <129635995+K7theCompSciMan@users.noreply.github.com>
2024-01-18 16:43:01 -0500

    initial code (#9)
    * fix: correct 2024 field image corners and field size (#1)
    
    * chore: add vendordeps (#2)
    
    * chore: upgrade photonlib (#5)
    
    * K7 (#6)
    
    * Updated CTRE work
    
    * Added More CTRE swerve work
    
    * chore: add vendordeps (#2) (#3)
    
    Co-authored-by: Nick Hadley <nick.hadley31@gmail.com>
    
    * chore: add vendordeps (#2) (#4)
    
    Co-authored-by: Nick Hadley <nick.hadley31@gmail.com>
    
    * Updated work on driving. Sim is still not working very well
    
    * More testing and work on sim. Swerve Modules are still fixed in place.
    
    * Updated PID and Pathplanner. Sim is now working
    
    * Updated PID and PathPlanner paths
    
    * Merge w main (#7)
    
    * chore: add vendordeps (#2)
    
    * chore: upgrade photonlib (#5)
    
    ---------
    
    Co-authored-by: Nick Hadley <nick.hadley31@gmail.com>
    
    ---------
    
    Co-authored-by: Nick Hadley <nick.hadley31@gmail.com>
    
    ---------
    
    Co-authored-by: Nick Hadley <nick.hadley31@gmail.com>

---
+++ src/main/java/frc/robot/io/DriverControls.java
@@ -0,0 +1,49 @@
+package frc.robot.io;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.DriverConstants;
+import frc.robot.Constants.SwerveConstants;
+
+public class DriverControls extends CommandXboxController {
+
+  public DriverControls(int port) {
+    super(port);
+  }
+
+  public double driveForward() {
+    return MathUtil.applyDeadband(-getLeftY(), DriverConstants.stickDeadband) * SwerveConstants.limit
+        * SwerveConstants.maxSpeedMPS;
+  }
+
+  public double driveStrafe() {
+    return MathUtil.applyDeadband(-getLeftX(), DriverConstants.stickDeadband) * SwerveConstants.limit
+        * SwerveConstants.maxSpeedMPS;
+  }
+
+  public double driveRotation() {
+    return MathUtil.applyDeadband(-getRightX(), DriverConstants.stickDeadband) * SwerveConstants.limit
+        * SwerveConstants.maxAngularVelocityRPS;
+  }
+
+  public Trigger toAmp(){
+    return povLeft();
+  }
+
+  public Trigger resetGyro() {
+    return start();
+  }
+
+  public Trigger robotRelative() {
+    return rightStick();
+  }
+
+  public Trigger increaseLimit() {
+    return rightBumper();
+  }
+
+  public Trigger decreaseLimit() {
+    return leftBumper();
+  }
+}
