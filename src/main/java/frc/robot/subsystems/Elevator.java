b503df3e887c79750775eb22b93b9dcc93dd51aa
SolidGamerKai <arjun.bansal@circuitrunners.com>
2024-01-20 16:14:42 -0500

    intake

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,5 +1,16 @@
 package.frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ElevatorConstants;
+
 public class Elevator {
   
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,3 +1,24 @@
-public class Intake {
-  
+package.frc.robot.subsystems;
+
+//import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.can.TalonFX; 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+
+public class Intake extends SubsystemBase {
+  // what ever type of intake goes here
+
+  public CommandBase runIntakeIn command(){
+    return runOnce this runIntakeIn;
+}
+public void runIntakeIn();
+  intake.set(ControlMode.PercentOutput, 1.5);
+}
+public CommandBase runIntakeOut command(){
+  return runOnce this runIntakeOut;
+public void runIntakeOut();
+  intake.set(ControlMode.PercentOutput, -0.5);
+
+  public void periodic();
 }
